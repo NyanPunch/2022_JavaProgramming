4f671062dc5cff6c99fdd052a66f2e0a3fdbdfd4
WhoLook <51149853+NyanPunch@users.noreply.github.com>
2022-09-21 20:11:48 +0900

    Update Average.java

--- HomeWork3/src/Average.java
+++ HomeWork3/src/Average.java
@@ -1,11 +1,14 @@
 //Exam3-11
 public class Average {
-    public static void main(String[] args) {
-        int sum = 0;
-
-        for(int i=0; i<args.length; i++) {
-            sum += Integer.parseInt(args[i]);
+    public static void main (String[] args) {
+        if(args.length == 0) {
+            System.out.println("args.length = 0");
+            return;
         }
-        System.out.printf("%d", sum/args.length);
+        int sum=0;
+        for (int i=0; i<args.length; i++) {
+            sum = sum + Integer.parseInt(args[i]);
+        }
+        System.out.println(sum/args.length);
     }
 }
